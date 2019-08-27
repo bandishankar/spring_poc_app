9062fa9e7ed5666941794ecd2622783e187df6ae
Bandi Shankar <bandishankar@gmail.com>
2019-08-27 15:29:18 -0400

    Update PocappTests.java

--- src/test/java/com/poc/app/PocappTests.java
+++ src/test/java/com/poc/app/PocappTests.java
@@ -8,9 +8,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PocappTests {
-
 	@Test
 	public void contextLoads() {
+		
 	}
 
 }
